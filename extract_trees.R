232f6a2f345f0feb28736611a05a36fd1c6254b9
AJP <aaron@redfoxgis.com>
2019-12-17 20:32:09 -0600

    added more comments

--- extract_trees.R
+++ extract_trees.R
@@ -5,8 +5,8 @@ require(matlab) # for tic() toc() function
 
 # define the las file
 data <- '/Users/aaron/gdrive/projects/ubc_project/data/las_2018/4810E_54570N.las'
+las <- readLAS(data, filter="-drop_class 1 3 4 6 7 8 9") # read las and keep class 2 (bare earth) and 5 (trees) classes
 writelax(data) # Create a spatial index file (.lax) to speed up processing
-las <- readLAS(data, filter="-drop_class 1 3 4 6 7 8 9")
 
 # Create a function to filter noise from point cloud
 # https://cran.r-project.org/web/packages/lidR/vignettes/lidR-catalog-apply-examples.html
@@ -24,7 +24,7 @@ las_denoised <- lasfilternoise(las, sensitivity = 1.2)
 # Inspect our las file
 lascheck(las)
 summary(las)
-#plot(las, color = "Classification") # Plot the points with colored classes
+plot(las, color = "Classification") # Plot the points with colored classes
 
 # Normalize data (try reading las file in using only ground points readlas())
 las_dtm <- readLAS(data, filter="-drop_class 1 3 4 5 6 7 8 9")
@@ -40,6 +40,16 @@ tic()
 chm  <- grid_canopy(las_normalized, 0.25, pitfree(c(0,2,5,10,15), c(0,1), subcircle = 0.2))
 toc()
 plot(chm)
+
+# smoothing post-process (e.g. two pass, 3x3 median convolution) ~745 seconds
+tic()
+ker = matrix(1,3,3)
+chm_s = focal(chm, w = ker, fun = median)
+chm_s = focal(chm, w = ker, fun = median)
+toc()
+
+plot(chm, col = height.colors(50)) # check the image
+writeRaster(chm_s,"/Users/aaron/Desktop/temp/ubc_temp/chm_med_filter.tif", options=c('TFW=YES'))
 
 # Watershed segmentation ~993 seconds
 tic()
@@ -60,8 +70,13 @@ toc()
 spplot(hulls, "zmax")
 
 # Subset hulls to retain polygons meeting certain criteria
-sub = subset(hulls, area <100)
+sub = subset(hulls, area <500)
 
 # Write to shapefile
-writeOGR(obj=hulls, dsn="/Users/aaron/Desktop/temp/ubc_temp", layer="hulls-bbox", driver="ESRI Shapefile") # this is in geographical projection
+writeOGR(obj=hulls, dsn="/Users/aaron/Desktop/temp/ubc_temp", layer="hulls-subset", driver="ESRI Shapefile") # this is in geographical projection
 
+# Detect trees
+# variable windows size
+f <- function(x) { x * 0.07 + 3}
+ttops <- tree_detection(las, lmf(f))
+writeOGR(obj=ttops, dsn="/Users/aaron/Desktop/temp/ubc_temp", layer="treetops", driver="ESRI Shapefile") 
