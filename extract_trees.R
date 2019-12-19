43fb4cb1521beffdb49f940698440ff5f7df8498
AJP <aaron@redfoxgis.com>
2019-12-18 23:00:57 -0600

    Tweaked the canopy height model to get better canopy hulls

--- extract_trees.R
+++ extract_trees.R
@@ -32,26 +32,29 @@ dtm <- grid_terrain(las_dtm, algorithm = knnidw(k = 8, p = 2))
 las_normalized <- lasnormalize(las_denoised, dtm)
 
 # Inspect the normalized las file
+lascheck(las_normalized)
 plot(las_normalized)
 summary(las_normalized)
 
-# Generate a normalized CHM ~675 seconds
+# Generate a normalized CHM ~198 seconds
+# https://github.com/Jean-Romain/lidR/wiki/Segment-individual-trees-and-compute-metrics
 tic()
-chm  <- grid_canopy(las_normalized, 0.25, pitfree(c(0,2,5,10,15), c(0,1), subcircle = 0.2))
+algo <- pitfree(thresholds = c(0,10,20,30,40,50), subcircle = 0.2)
+chm  <- grid_canopy(las_normalized, 0.5, algo)
 toc()
 plot(chm)
 
-# smoothing post-process (e.g. two pass, 3x3 median convolution) ~745 seconds
+# smoothing post-process (e.g. two pass, 3x3 median convolution) ~220 seconds
 tic()
-ker = matrix(1,3,3)
-chm_s = focal(chm, w = ker, fun = median)
-chm_s = focal(chm, w = ker, fun = median)
+ker <- matrix(1,3,3)
+chm_s <- focal(chm, w = ker, fun = median)
+chm_s <- focal(chm, w = ker, fun = median)
 toc()
 
 plot(chm, col = height.colors(50)) # check the image
-writeRaster(chm_s,"/Users/aaron/Desktop/temp/ubc_temp/chm_med_filter.tif", options=c('TFW=YES')) # Sanity check
+writeRaster(chm_s,"/Users/aaron/Desktop/temp/ubc_temp/chm_med_filter_0p5.tif", options=c('TFW=YES')) # Sanity check
 
-# Watershed segmentation ~993 seconds
+# Watershed segmentation ~68 seconds
 tic()
 algo <- watershed(chm, th = 4)
 las_watershed  <- lastrees(las_normalized, algo)
@@ -63,17 +66,17 @@ trees <- lasfilter(las_watershed, !is.na(treeID))
 # View the results
 plot(trees, color = "treeID", colorPalette = pastel.colors(100))
 
-# Generate tree hulls and standard metrics ~29 seconds
+# Generate tree hulls and standard metrics ~39 seconds
 tic()
 hulls  = tree_hulls(trees, type = "convex", func = .stdmetrics)
 toc()
 spplot(hulls, "zmax")
 
-# Subset hulls to retain polygons meeting certain criteria
-sub = subset(hulls, area <500)
+# Subset hulls to retain polygons meeting certain criteria (This filters out error polygons)
+sub = subset(hulls, area <1200 & area > 3)
 
 # Write to shapefile (Sanity check)
-writeOGR(obj=hulls, dsn="/Users/aaron/Desktop/temp/ubc_temp", layer="hulls-subset", driver="ESRI Shapefile") # this is in geographical projection
+writeOGR(obj=sub, dsn="/Users/aaron/Desktop/temp/ubc_temp", layer="hulls-subset-0p5-v3", driver="ESRI Shapefile") # this is in geographical projection
 
 # Detect trees (Optional, just to get tree centers)
 # variable windows size
